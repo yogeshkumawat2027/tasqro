3dfbe38a60b11f4491ce19d70f370d4471f392be
yogeshkumawat2027 <yogeshkumawat2027@gmail.com>
2026-06-23 13:15:47 +0530

    added more 3 controller for board add , get , remove memeber

--- backend/controllers/boardController.js
+++ backend/controllers/boardController.js
@@ -38,8 +38,6 @@ export const getSingleBoard = asyncHandler(async (req, res) => {
    await  board.populate("createdBy", "name email role")
          .populate("members", "name email role");
 
-  
-
   if (!board.members.some((m) => m._id.toString() === req.user._id.toString())) {
     return res.status(403).json({
       success: false,
@@ -56,20 +54,16 @@ export const getSingleBoard = asyncHandler(async (req, res) => {
 export const updateBoard = asyncHandler(async (req, res) => {
   const { title, description } = req.body;
 
-
   if(title !== undefined && title.trim() === "") 
     return res.status(400).json({success: false,message: "Title cannot be empty",});
 
-
   const board = await Board.findById(req.params.id);
-
   if(!board) return res.status(404).json({success: false,message: "Board not found",}); 
 
-  if(board.createdBy.toString() !== req.user._id.toString() &&req.user.role !== "admin"){  //user or admin can update
-    
+  if(board.createdBy.toString() !== req.user._id.toString()){  //board creator can update
     return res.status(403).json({
       success: false,
-      message: "Only board creator or admin can update board",
+      message: "Only board creator can update board",
     });
   }
 
@@ -86,16 +80,14 @@ export const updateBoard = asyncHandler(async (req, res) => {
 });
 
 export const deleteBoard = asyncHandler(async (req, res) => {
-  const board = await Board.findById(req.params.id);
 
+  const board = await Board.findById(req.params.id);
   if(!board)  return res.status(404).json({success: false,message: "Board not found",});
-   
-  if(board.createdBy.toString() !== req.user._id.toString() &&
-    req.user.role !== "admin"){
-  
+
+  if(board.createdBy.toString() !== req.user._id.toString()){
     return res.status(403).json(
         {success: false,
-         message: "Only board creator or admin can delete board" 
+         message: "Only board createor can delete board" 
         }
     ); 
   }
@@ -106,4 +98,67 @@ export const deleteBoard = asyncHandler(async (req, res) => {
     success: true,
     message: "Board deleted successfully", deleted
   });
+});
+
+export const addBoardMember = asyncHandler(async (req, res) =>{
+  const { email } = req.body;
+
+  const board = await Board.findById(req.params.id);
+  if (!board) return res.status(404).json({ success: false, message: "Board not found" });
+
+  if (board.createdBy.toString() !== req.user._id.toString()) {
+    return res.status(403).json({ success: false, message: "Only board creator can add members" });
+  }
+
+  const user = await User.findOne({ email });
+  if(!user) return res.status(404).json({ success: false, message: "User not found" });
+
+  if (board.members.includes(user._id)) {
+    return res.status(400).json({ success: false, message: "User already member" });
+  }
+
+  board.members.push(user._id);
+  await board.save();
+
+  res.status(200).json({ success: true, message: "Member added successfully", board });
+});
+
+export const removeBoardMember = asyncHandler(async (req, res) =>{
+
+  const board = await Board.findById(req.params.id);
+  if (!board) return res.status(404).json({ success: false, message: "Board not found" });
+
+  if (board.createdBy.toString() !== req.user._id.toString()) {
+    return res.status(403).json({ success: false, message: "only board creator can remove members"});
+  }
+
+  if (req.params.userId === board.createdBy.toString()) {
+    return res.status(400).json({ success: false, message: "Creator cannot be removed" });
+  }
+
+  board.members = board.members.filter(
+    (memberId) => memberId.toString() !== req.params.userId
+  );
+
+  await board.save();
+
+  res.status(200).json({ success: true, message: "member removed successfully", board });
+});
+
+
+export const getBoardMembers = asyncHandler(async (req, res) => {
+
+  const board = await Board.findById(req.params.id).populate( "members","name email role" );
+  
+  if (!board) return res.status(404).json({ success: false, message: "Board not found" });
+
+  const isMember = board.members.some(
+    (m) => m._id.toString() === req.user._id.toString()
+  );
+
+  if (!isMember) {
+    return res.status(403).json({ success: false, message: "You are not a member of this board" });
+  }
+
+  res.status(200).json({ success: true, members: board.members });
 });
