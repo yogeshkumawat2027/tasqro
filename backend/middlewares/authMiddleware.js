ae745edb8c2a71e1e1b8cb46f826c6b78f478f69
yogeshkumawat2027 <yogeshkumawat2027@gmail.com>
2026-06-23 12:02:44 +0530

    resolve auth bug

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../models/User";
+import User from "../models/User.js";
 import { decode } from "node:punycode";
 
 const auth = async (req , res , next)=>{
