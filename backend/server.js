deaec5276ecd8e5c4196ea1e2e672c36f3a7e167
yogeshkumawat2027 <yogeshkumawat2027@gmail.com>
2026-06-24 11:39:38 +0530

    getting error due to cookie parser install and fix it

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ import express from "express";
 
 import dotenv from "dotenv";
 
+import cookieParser from "cookie-parser";
+
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import boardRoutes from "./routes/boardRoutes.js";
+import cardRoutes from "./routes/cardRoutes.js";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -12,15 +15,14 @@ const app = express();
 app.use(express.json());
 
 dotenv.config();
-
-
-
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/boards", boardRoutes);
+app.use("/api/cards", cardRoutes);
 
 
-app.get("/" , (req,res)=>{
+app.get("/", (req,res)=>{
     res.json({ message : "api is running"});
 });
  
