daed1730e1ffbd691fae86b6cf8a68a6c49637a1
yogeshkumawat2027 <yogeshkumawat2027@gmail.com>
2026-06-25 11:38:53 +0530

    react setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Mini Trello</h1>} />
+    </Routes>
+  );
+}
+
+export default App;
+
