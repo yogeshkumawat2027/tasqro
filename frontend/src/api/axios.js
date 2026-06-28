4e4fe0c1b45002e021165664f63ba904a29b4801
yogeshkumawat2027 <yogeshkumawat2027@gmail.com>
2026-06-28 12:20:48 +0530

    added base url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://azentrix-fullstack-task2-w51b.onrender.com/api",
   withCredentials: true,
 });
 
