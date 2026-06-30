13d919413e00fb229a7b1f0ac3b3dd63cefc8dd2
yogeshkumawat2027 <yogeshkumawat2027@gmail.com>
2026-06-30 20:43:07 +0530

    added cors origin in backend to connect frontend

--- backend/socket.js
+++ backend/socket.js
@@ -5,7 +5,10 @@ let io;
 export const initSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+     origin: [
+      "http://localhost:5173",
+      "https://azentrix-fullstack-task2-eta.vercel.app",
+    ],
       credentials: true,
     },
   });
