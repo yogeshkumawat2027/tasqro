bf5503e9a75d5d4669795b96bbd5b7cf277b77d3
yogeshkumawat2027 <yogeshkumawat2027@gmail.com>
2026-07-06 13:48:25 +0530

    refactor server setup; move express app configuration to app.js for better modularity  for testing purpose

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import authRoutes from "./routes/authRoutes.js";
+import boardRoutes from "./routes/boardRoutes.js";
+import cardRoutes from "./routes/cardRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://azentrix-fullstack-task2-eta.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.json({ message: "api is running" });
+});
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Backend is running",
+  });
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/boards", boardRoutes);
+app.use("/api/cards", cardRoutes);
+app.use("/api/admin", adminRoutes);
+
+export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,17 @@
-import express from "express";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import http from "http";
-import cors from "cors";
+dotenv.config();
 
+import http from "http";
+
+import app from "./app.js";
 import connectDB from "./config/db.js";
-import authRoutes from "./routes/authRoutes.js";
-import boardRoutes from "./routes/boardRoutes.js";
-import cardRoutes from "./routes/cardRoutes.js";
-import adminRoutes from "./routes/adminRoutes.js";
 import { initSocket } from "./socket.js";
 
 const PORT = process.env.PORT || 5000;
-const app = express();
-
-
-app.use(
-    cors({
-        origin: [
-            "http://localhost:5173",
-            "https://azentrix-fullstack-task2-eta.vercel.app"
-        ],
-        credentials: true,
-    })
-);
-app.use(express.json());
-app.use(cookieParser());
-dotenv.config();
-
-app.use("/api/auth", authRoutes);
-app.use("/api/boards", boardRoutes);
-app.use("/api/cards", cardRoutes);
-app.use("/api/admin", adminRoutes);
-
-
-app.get("/", (req, res) => {
-    res.json({ message: "api is running" });
-});
 
 const server = http.createServer(app);
-initSocket(server);
 
+initSocket(server);
 
 connectDB().then(() => {
   server.listen(PORT, () => {
